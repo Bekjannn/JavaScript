261d9311b9e3f6df7ce1413445d40e96600b046f
Bekjannn <bekmahnudov@gmail.com>
2023-01-28 23:40:23 +0500

    paper

---
+++ paper/main.js
@@ -0,0 +1,135 @@
+
+
+
+// function bolinuvchi() {
+//     let sum = 0
+//     for (let i = 1; i < 100; i++) {
+//         if (i % 3 == 0 && i % 5 == 0) {
+//             console.log(i);
+//             sum = sum + i
+//         }
+//     }
+//     console.log(sum);
+// }
+
+// ######################################################
+
+// let bushMassiv = []
+
+// function movie() {
+
+//     for (let i = 0; i < movies.length; i++) {
+//         if (movies[i].item_count >= 1000) {
+//             bushMassiv.push(movies[i])
+//         }
+//     }
+//     console.log(bushMassiv);
+
+// }
+
+// #####################################################
+
+// let enFilms = []
+// let itFilms = []
+// let feFilms = []
+// let esFilms = []
+// let deFilms = []
+// let huFilms = []
+
+// function en() {
+
+//     for (let i = 0; i < movies.length; i++) {
+//         if (movies[i].iso_639_1 == "en") {
+//             enFilms.push(movies[i])
+//         }
+//     }
+//     console.log(enFilms);
+
+// }
+// function it() {
+
+//     for (let i = 0; i < movies.length; i++) {
+//         if (movies[i].iso_639_1 == "it") {
+//             itFilms.push(movies[i])
+//         }
+//     }
+//     console.log(itFilms);
+
+// }
+// function fe() {
+
+//     for (let i = 0; i < movies.length; i++) {
+//         if (movies[i].iso_639_1 == "fe") {
+//             feFilms.push(movies[i])
+//         }
+//     }
+//     console.log(feFilms);
+
+// }
+// function es() {
+
+//     for (let i = 0; i < movies.length; i++) {
+//         if (movies[i].iso_639_1 == "es") {
+//             enFilms.push(movies[i])
+//         }
+//     }
+//     console.log(esFilms);
+
+// }
+// function de() {
+
+//     for (let i = 0; i < movies.length; i++) {
+//         if (movies[i].iso_639_1 == "de") {
+//             deFilms.push(movies[i])
+//         }
+//     }
+//     console.log(deFilms);
+
+// }
+// function hu() {
+
+//     for (let i = 0; i < movies.length; i++) {
+//         if (movies[i].iso_639_1 == "hu") {
+//             huFilms.push(movies[i])
+//         }
+//     }
+//     console.log(huFilms);
+
+// }
+
+
+// ########################################################
+
+// let massiv = []
+
+// function Tomoshalar() {
+
+//     for (let i = 0; i < movies.length; i++) {
+//         if (movies[i].item_count >= 500 && movies[i].item_count <= 1000) {
+//             massiv.push(movies[i])
+//         }
+//     }
+//     console.log(massiv);
+
+// }
+
+// #########################################################
+
+
+
+
+// let bor = []
+// let yuq = []
+
+// function Surat() {
+//     for (let i = 0; i < movies; i++) {
+//         if (movies[i].poster_path.length > 0) {
+//             bor.push(moviesni[i])
+//         }
+        // if (movies[i].poster_path == 0) {
+        //     yuq.push(movies[i])
+        // }
+//     }
+//     console.log(bor);
+    // console.log(yuq);
+// }
