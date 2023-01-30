834badd9d4ef11467328b9f866ba78803a953d54
Bekjannn <bekmahnudov@gmail.com>
2023-01-30 16:30:43 +0500

    asdasd

--- 2-dars/homework/homework.js
+++ 2-dars/homework/homework.js
@@ -1,1 +1,50 @@
-// awresthyuiopiudsdfghmjkljhgjf
+// homework
+
+// 2-prefix and postfix
+
+// let a = 1, b = 1;
+
+// let c = ++a; //   2 chiqadi chunki c ni 1+a ning qiymatiga tenglayabdi ya'ni 1+1
+// let d = b++; //   1 chiqadi chunki d ni b ning qiymatiga tenglaydi va javobni consolga uzatadi keyin 1 ni qo'shadi agar d ning qiymati 2- marta ham consolcha chiqarilsa 1+1 ya'ni 2 qiymatini korsatadi
+
+
+// console.log(c);
+// console.log(d);
+
+//  3-Assinment result
+
+// let a = 2;
+
+// let x = 1 + (a *= 2); //  5 bu holatda x ga qiynat olishda avval a (a=2) ning kvadratini hisoblaydi va 1 ni qo'shoib 5 javob qaytaradi.
+
+// console.log(x);
+
+
+//  4-Type Conversion
+
+// console.log("" + 1 + 0); //
+// console.log("" - 1 + 0);//
+// console.log(true + false);  // 1 chunki :true= 1, false=0. 1 + 0 = 1   
+// console.log(6 / "3"); //
+// console.log("2" * "3"); //
+// console.log(4 + 5 + "px"); // 9px bu holatda 2 ta numberni qo'shib keyin stringni yoniga qo'shib qo'yadi
+// console.log("$" + 4 + 5); 
+// console.log("4" - 2);
+// console.log("4px" - 2);
+// console.log("  -9  " + 5);
+// console.log("  -9  " - 5);
+// console.log(null + 1); //  1 chunki null ning qiymati 0 ga teng
+// console.log(undefined + 1);
+// console.log(" \t \n" - 2);
+
+
+
+//  5-Comparisons
+
+// console.log(5 > 4); //true
+// console.log("apple" > "pineapple"); // false
+// console.log("2" > "12"); //  true
+// console.log(undefined == null);  // true
+// console.log(undefined === null); //  false
+// console.log(null == "\n0\n"); //  false
+// console.log(null === + "\n0\n"); //  false
